86e85b4b16f1593e831889af6ba47b3b236702b1
Brannon <57905614+BrannonOpperman@users.noreply.github.com>
2019-12-07 20:57:33 +0200

    Added delete onClick functionality

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ class App extends Component {
   state = {
     todos: [
       { id: 1, content: 'Buy some milk' },
-      { id: 2, content: 'Play some mario kart' }
-    ]
+      { id: 2, content: 'Play some mario kart' }]
+  }
+  deleteTodo = (id) => {
+    // Creates a new array containing all the elements minus the object whose id matches the one clicked
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    // Sets the new array to be equal to the array created above
+    this.setState({
+      // Can just use 'todos' as shorthand too because they have the same name
+      todos: todos
+    })
   }
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
